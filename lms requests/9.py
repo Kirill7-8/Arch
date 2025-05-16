abbbe49ff53ada00e48ed8ef24e8e72d703d23bc
Kirill <obrazkovk@yandex.ru>
2025-05-17 02:27:52 +0300

    добавил 9 задание

--- lms requests/9.py
+++ lms requests/9.py
@@ -1,2 +1,7 @@
-import sys
-print({x.split("=")[0]: x.split("=")[1].strip() for x in sys.stdin})
+from requests import put
+import json
+import sys 
+url = f"http://{input()}/users/{input()}"
+data = json.dumps({x.split("=")[0]: x.split("=")[1].strip() for x in sys.stdin})
+put(url, data)
+
